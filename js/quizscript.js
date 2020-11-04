d79408dd90227db3c69ea5c390d4bcf5a92cc3c4
Agata Lubanska <swistaczek@Agatas-MacBook-Pro.local>
2020-11-04 22:08:37 +0100

    lol

--- js/quizscript.js
+++ js/quizscript.js
@@ -1,1 +1,35 @@
-document.querySelector
+window.addEventListener("load", loadQuiz);
+
+function loadQuiz() {
+    console.log("loadQuiz");
+
+    document.querySelector(".next").addEventListener("click", questionOne);
+    document.querySelector("#two").classList.add("display_none");
+    document.querySelector("#two").addEventListener("click", questionTwo);
+    document.querySelector("#three").classList.add("display_none");
+    document.querySelector("#three").addEventListener("click", questionThree);
+    document.querySelector("#four").classList.add("display_none");
+    document.querySelector("#four").addEventListener("click", questionFour);
+    document.querySelector("#five").classList.add("display_none");
+
+}
+
+function questionOne() {
+    document.querySelector("#one").classList.add("display_none");
+    document.querySelector("#two").classList.remove("display_none");
+}
+
+function questionTwo() {
+    this.classList.add("display_none");
+    document.querySelector("#three").classList.remove("display_none");
+}
+
+function questionThree() {
+    this.classList.add("display_none");
+    document.querySelector("#four").classList.remove("display_none");
+}
+
+function questionFour() {
+    this.classList.add("display_none");
+    document.querySelector("#five").classList.remove("display_none");
+}
