8daa6bb053eab111d04797dea3d183cfd1056406
AgataLub <70294512+AgataLub@users.noreply.github.com>
2020-11-13 18:07:27 +0100

    quiz

--- js/quizscript.js
+++ js/quizscript.js
@@ -17,112 +17,114 @@ function loadQuiz() {
     document.querySelector("#four").classList.add("display_none");
     document.querySelector("#five").classList.add("display_none");
 
-}
+    var slider = document.querySelector("#myRange");
+    var output = document.querySelector("#demo");
+    output.innerHTML = slider.value;
 
-function questionZero() {
-    document.querySelector("#two").classList.add("display_none");
-    document.querySelector("#one").classList.remove("display_none");
-    document.querySelector(".orange_bar").classList.remove("transform40");
-}
-
-
-function questionOne() {
-    document.querySelector("#one").classList.add("display_none");
-    document.querySelector("#three").classList.add("display_none");
-
-    document.querySelector("#two").classList.remove("display_none");
-
-    document.querySelector(".orange_bar").classList.add("transform40");
-    document.querySelector(".orange_bar").classList.remove("transform60");
-}
-
-function questionTwo() {
-    document.querySelector("#two").classList.add("display_none");
-    document.querySelector("#four").classList.add("display_none");
-
-    document.querySelector("#three").classList.remove("display_none");
-
-    document.querySelector(".orange_bar").classList.remove("transform80");
-    document.querySelector(".orange_bar").classList.add("transform60");
-}
-
-function questionThree() {
-    document.querySelector("#three").classList.add("display_none");
-    document.querySelector("#four").classList.remove("display_none");
-
-    document.querySelector(".orange_bar").classList.add("transform80");
-}
-
-function questionFour() {
-    document.querySelector("#four").classList.add("display_none");
-    document.querySelector("#five").classList.remove("display_none");
-
-    document.querySelector("#loading").classList.add("display_none");
-}
-
-
-//Slider
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-    output.innerHTML = this.value;
-}
-
-//Fill fields
-
-fetchQuestions();
-
-function fetchQuestions() {
-    console.log("fetchQuestion");
-    fetch("https://agataswistak.com/wordpress/wp-json/wp/v2/ap-quiz")
-        .then(function (response) {
-            console.log(response)
-            return response.json();
-        })
-
-        .then(function (data) {
-            console.log("data")
-            dataReceived(data);
-        })
-}
-
-function dataReceived(question) {
-    question.forEach(showQuestion);
-    console.log("data received");
-}
-
-
-function showQuestion(myQuestion) {
-    console.log("myQuestion")
-
-    var temp = document.querySelector("#templateID");
-    var myCopy = temp.cloneNode(true);
-
-    myCopy.querySelector(".number_span").textContent = myQuestion.number;
-    myCopy.querySelector("h1").textContent = myQuestion.quiz_title;
-    myCopy.querySelector("p").textContent = myQuestion.blurb;
-    myCopy.querySelector("#specificQuestion").textContent = myQuestion.question;
-
-    if (myQuestion.number < 4) {
-        myCopy.querySelector(".table").classList.remove("display_none");
-        myCopy.querySelector("#answer1").textContent = myQuestion.answers;
-        myCopy.querySelector("#answer2").textContent = myQuestion.answers2;
-        myCopy.querySelector("#answer3").textContent = myQuestion.answers3;
-        myCopy.querySelector("#answer4").textContent = myQuestion.answers4;
-        myCopy.querySelector("#answer5").textContent = myQuestion.answers5;
-
-    } else {
-        myCopy.querySelector(".slidercontainer").classList.remove("display_none");
+    slider.oninput = function() {
+        console.log("This.value:" + this.value + "output.innerHTML" + output.innerHTML);
+        output.innerHTML = this.value;
     }
 
-    var test = myQuestion.number;
-    console.log(test);
-    console.log('#template_place' + myQuestion.number);
+    function questionZero() {
+        document.querySelector("#two").classList.add("display_none");
+        document.querySelector("#one").classList.remove("display_none");
+        document.querySelector(".orange_bar").classList.remove("transform40");
+    }
 
-    const parentElem = document.querySelector("#template_place" + myQuestion.number);
-    parentElem.appendChild(myCopy);
+
+    function questionOne() {
+        document.querySelector("#one").classList.add("display_none");
+        document.querySelector("#three").classList.add("display_none");
+
+        document.querySelector("#two").classList.remove("display_none");
+
+        document.querySelector(".orange_bar").classList.add("transform40");
+        document.querySelector(".orange_bar").classList.remove("transform60");
+    }
+
+    function questionTwo() {
+        document.querySelector("#two").classList.add("display_none");
+        document.querySelector("#four").classList.add("display_none");
+
+        document.querySelector("#three").classList.remove("display_none");
+
+        document.querySelector(".orange_bar").classList.remove("transform80");
+        document.querySelector(".orange_bar").classList.add("transform60");
+    }
+
+    function questionThree() {
+        document.querySelector("#three").classList.add("display_none");
+        document.querySelector("#four").classList.remove("display_none");
+
+        document.querySelector(".orange_bar").classList.add("transform80");
+    }
+
+    function questionFour() {
+        document.querySelector("#four").classList.add("display_none");
+        document.querySelector("#five").classList.remove("display_none");
+
+        document.querySelector("#loading").classList.add("display_none");
+    }
+
+    //Fill fields
+
+    fetchQuestions();
+
+    function fetchQuestions() {
+        console.log("fetchQuestion");
+        fetch("https://agataswistak.com/wordpress/wp-json/wp/v2/ap-quiz")
+            .then(function (response) {
+                console.log(response)
+                return response.json();
+            })
+
+            .then(function (data) {
+                console.log("data")
+                dataReceived(data);
+            })
+    }
+
+    function dataReceived(question) {
+        question.forEach(showQuestion);
+        console.log("data received");
+    }
+
+
+    function showQuestion(myQuestion) {
+        console.log("myQuestion")
+
+        var temp = document.querySelector("#templateID");
+        var myCopy = temp.cloneNode(true);
+
+        myCopy.querySelector(".number_span").textContent = myQuestion.number;
+        myCopy.querySelector("h1").textContent = myQuestion.quiz_title;
+        myCopy.querySelector("p").textContent = myQuestion.blurb;
+        myCopy.querySelector("#specificQuestion").textContent = myQuestion.question;
+
+        if (myQuestion.number < 4) {
+            myCopy.querySelector(".table").classList.remove("display_none");
+            myCopy.querySelector("#answer1").textContent = myQuestion.answers;
+            myCopy.querySelector("#answer2").textContent = myQuestion.answers2;
+            myCopy.querySelector("#answer3").textContent = myQuestion.answers3;
+            myCopy.querySelector("#answer4").textContent = myQuestion.answers4;
+            myCopy.querySelector("#answer5").textContent = myQuestion.answers5;
+
+        } else {
+            myCopy.querySelector(".slidecontainer").classList.remove("display_none");
+
+        }
+
+        var myQuestionImage = myQuestion.image_quiz.guid;
+        console.log(myQuestionImage);
+
+        myCopy.querySelector(".illustration").setAttribute("src", myQuestionImage);
+
+        var test = myQuestion.number;
+        console.log(test);
+        console.log('#template_place' + myQuestion.number);
+
+        const parentElem = document.querySelector("#template_place" + myQuestion.number);
+        parentElem.appendChild(myCopy);
+    }
 }
